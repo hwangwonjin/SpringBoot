396f95b868f231cd2fbfd2b1bac7fa157b98a223
Hwangwonjin <ghkddnjs1269@gmail.com>
2023-01-10 14:39:51 +0900

    2023/01/10

---
+++ User/src/main/java/kr/co/user/vo/User1VO.java
@@ -0,0 +1,14 @@
+package kr.co.user.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User1VO {
+
+	private String uid;
+	private String name;
+	private String hp;
+	private int age;
+}
